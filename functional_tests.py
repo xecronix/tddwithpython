e83fadce05f34296481fff2a53d73307d188c8f3
ronald <ronald@localhost.localdomain>
2016-02-01 21:27:57 -0500

    restructured functional_tests.py to a test class.  Added user story to as comments in the class.

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python3
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http:localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+	
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		
+		# Edith wants to check out a new online to-do app
+		self.browser.get('http:localhost:8000')
 
-assert 'Django' in browser.title
+		# She notices the page title mentions to-do lists
+		self.assertIn('To-Do', self.browser.title)
+
+		# She's invited to enter a to-do item
+
+		# She enters the to-do item "Buy Peacock fethers" into a textbox
+
+		# When she hits enter, the page updates and now the page lists
+		# "1: Buy peacock feathers"
+
+		# there is still a textbox inviting her to enter more items. 
+
+		# She adds a second item "Use peacock fethers to make a fishing fly"
+
+		# The page updates again and now she sees both to-do items.
+
+		# Edith notices instructions for bookmarking a URL where her list can 
+		# always be retrieved.
+
+		# She visits the URL to see her list is actually available.
+
+		# Exhausted, she goes to bed.
+		self.fail("Finish the test")
+
+if __name__ == '__main__':
+	unittest.main()
